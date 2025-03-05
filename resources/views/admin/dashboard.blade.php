14181a04f072e3f1347598c326b3968947755500
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-05 13:23:10 +0100

    feat: replaced default dashboard view

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,17 +1,66 @@
 <x-admin-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            {{ __('Admin Dashboard') }}
-        </h2>
-    </x-slot>
-
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("You're logged in!") }}
+    <div class="py-6 max-w-[95%] mx-auto">
+        <div class="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+            <article class="group flex rounded-md place-items-center overflow-hidden border border-outline bg-surface-alt text-on-surface dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark shadow p-2">
+                <div class="bg-blue-500 dark:bg-surface-dark w-auto h-auto p-6 rounded-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
                 </div>
-            </div>
+                <div class="flex flex-col pl-4">
+                    <h3 class="text-balance text-md text-on-surface-strong dark:text-on-surface-dark-strong" aria-describedby="featureDescription">
+                        Total Admins
+                    </h3>
+                    <p id="featureDescription" class="text-pretty font-bold text-xl">
+                        1
+                    </p>
+                </div>
+            </article>
+            <article class="group flex rounded-md place-items-center overflow-hidden border border-outline bg-surface-alt text-on-surface dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark shadow p-2">
+                <div class="bg-blue-500 dark:bg-surface-dark w-auto h-auto p-6 rounded-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                </div>
+                <div class="flex flex-col pl-4">
+                    <h3 class="text-balance text-md text-on-surface-strong dark:text-on-surface-dark-strong" aria-describedby="featureDescription">
+                        News
+                    </h3>
+                    <p id="featureDescription" class="text-pretty font-bold text-xl">
+                        42
+                    </p>
+                </div>
+            </article>
+            <article class="group flex rounded-md place-items-center overflow-hidden border border-outline bg-surface-alt text-on-surface dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark shadow p-2">
+                <div class="bg-blue-500 dark:bg-surface-dark w-auto h-auto p-6 rounded-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                </div>
+                <div class="flex flex-col pl-4">
+                    <h3 class="text-balance text-md text-on-surface-strong dark:text-on-surface-dark-strong" aria-describedby="featureDescription">
+                        Reports
+                    </h3>
+                    <p id="featureDescription" class="text-pretty font-bold text-xl">
+                        1337
+                    </p>
+                </div>
+            </article>
+            <article class="group flex rounded-md place-items-center overflow-hidden border border-outline bg-surface-alt text-on-surface dark:border-outline-dark dark:bg-surface-dark-alt dark:text-on-surface-dark shadow p-2">
+                <div class="bg-blue-500 dark:bg-surface-dark w-auto h-auto p-6 rounded-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+                    </svg>
+                </div>
+                <div class="flex flex-col pl-4">
+                    <h3 class="text-balance text-md text-on-surface-strong dark:text-on-surface-dark-strong" aria-describedby="featureDescription">
+                        Online Users
+                    </h3>
+                    <p id="featureDescription" class="text-pretty font-bold text-xl">
+                        47
+                    </p>
+                </div>
+            </article>
         </div>
     </div>
 </x-admin-layout>
