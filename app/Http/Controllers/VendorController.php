d632f2f124fcd0f364e57796b14aeefb9d48913b
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-05 10:22:02 +0100

    feat: added default vendor layout

---
+++ app/Http/Controllers/VendorController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class VendorController extends Controller
+{
+    public function dashboard()
+    {
+        return view('vendor.dashboard');
+    }
+}
