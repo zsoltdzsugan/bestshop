aa4f3e4be3fe06e52c95d950cd1ac5683b134743
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-05 12:28:38 +0100

    feat: separated routing for admin/vendor/web

---
+++ routes/vendor.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\VendorController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::middleware(['auth', 'role:vendor'])
+    ->prefix('vendor')
+    ->as('vendor.')
+    ->group(function () {
+        Route::get('/dashboard', [VendorController::class, 'dashboard'])->name('dashboard');
+    });
