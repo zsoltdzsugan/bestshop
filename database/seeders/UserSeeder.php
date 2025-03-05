5c51e6202142743e4c56890c46031cb4c1aa4937
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-05 09:51:03 +0100

    feat: added UserSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'Test Admin',
+                'username' => 'admin',
+                'email' => 'admin@test.com',
+                'role' => 'admin',
+                'status' => 'active',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name' => 'Test Vendor',
+                'username' => 'vendor',
+                'email' => 'vendor@test.com',
+                'role' => 'vendor',
+                'status' => 'active',
+                'password' => bcrypt('password'),
+            ],
+            [
+                'name' => 'Test User',
+                'username' => 'user',
+                'email' => 'user@test.com',
+                'role' => 'user',
+                'status' => 'active',
+                'password' => bcrypt('password'),
+            ],
+        ]);
+    }
+}
