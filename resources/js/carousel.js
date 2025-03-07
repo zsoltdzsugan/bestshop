513e5c9518c2c164cd190b179bfdcadc3c829b40
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-07 12:23:22 +0100

    feat: added products carousel

--- resources/js/carousel.js
+++ resources/js/carousel.js
@@ -1,5 +1,5 @@
 document.addEventListener('alpine:init', () => {
-    Alpine.data('carouselData', () => ({
+    Alpine.data('carouselSlides', () => ({
         slides: [],
         currentSlideIndex: 1,
         autoplayIntervalTime: 4000,
@@ -21,6 +21,47 @@ document.addEventListener('alpine:init', () => {
                 this.currentSlideIndex = this.currentSlideIndex + 1;
             } else {
                 this.currentSlideIndex = 1;
+            }
+        },
+        autoplay() {
+            this.autoplayInterval = setInterval(() => {
+                if (!this.isPaused) {
+                    this.next();
+                }
+            }, this.autoplayIntervalTime);
+        },
+        setAutoplayInterval(newIntervalTime) {
+            clearInterval(this.autoplayInterval);
+            this.autoplayIntervalTime = newIntervalTime;
+            this.autoplay();
+        },
+    }));
+
+    Alpine.data('carouselProducts', () => ({
+        products: [],
+        currentProductIndex: 0,
+        productCount: 3,
+        autoplayIntervalTime: 5000,
+        autoplayInterval: null,
+        isPaused: false,
+        initializeProducts() {
+            this.products = JSON.parse(this.$el.getAttribute('data-products'));
+            this.autoplay();
+        },
+        previous() {
+            if (this.currentProductIndex > 0) {
+                this.currentProductIndex = this.currentProductIndex - this.productCount;
+            } else {
+                // If it's the first set of cards, go to the last set
+                this.currentProductIndex = (this.products.length - this.productCount);
+            }
+        },
+        next() {
+            if (this.currentProductIndex + this.productCount < this.products.length) {
+                this.currentProductIndex = this.currentProductIndex + this.productCount;
+            } else {
+                // If it's the last set of cards, go to the first set
+                this.currentProductIndex = 0;
             }
         },
         autoplay() {
