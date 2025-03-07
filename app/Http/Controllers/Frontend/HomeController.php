db1727e61736c734c4d538a2613081655d68dab4
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-07 12:22:20 +0100

    feat: added products

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -10,36 +10,9 @@ class HomeController extends Controller
 {
     public function index(): View
     {
-        $slides = [
-            [
-                'imgSrc' => 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-1.webp',
-                'imgAlt' => 'Vibrant abstract painting with swirling blue and light pink hues on a canvas.',
-                'title' => 'New Arrivals',
-                'description' => 'Men\'s Fashion',
-                'subtitle' => 'Starts At $99.0',
-                'ctaUrl' => 'https://example.com',
-                'ctaText' => 'Shop Now',
-            ],
-            [
-                'imgSrc' => 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-2.webp',
-                'imgAlt' => 'Vibrant abstract painting with swirling red, yellow, and pink hues on a canvas.',
-                'title' => 'New Arrivals',
-                'description' => 'Women\'s Fashion',
-                'subtitle' => 'Starts At $99.0',
-                'ctaUrl' => 'https://example.com',
-                'ctaText' => 'Shop Now',
-            ],
-            [
-                'imgSrc' => 'https://penguinui.s3.amazonaws.com/component-assets/carousel/default-slide-3.webp',
-                'imgAlt' => 'Vibrant abstract painting with swirling blue and purple hues on a canvas.',
-                'title' => 'New Arrivals',
-                'description' => 'Kid\'s Fashion',
-                'subtitle' => 'Starts At $99.0',
-                'ctaUrl' => 'https://example.com',
-                'ctaText' => 'Shop Now',
-            ],
-        ];
+        $slides = config('slides.home');
+        $products = config('products.products');
 
-        return view('public.home.home', compact('slides'));
+        return view('public.home.home', compact(['slides', 'products']));
     }
 }
