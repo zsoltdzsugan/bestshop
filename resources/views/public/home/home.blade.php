5fd4d9f956da1f144c91f2968d5f3f9b81949f89
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-07 04:06:03 +0100

    refactor: changed to public

---
+++ resources/views/public/home/home.blade.php
@@ -0,0 +1,10 @@
+<x-public-layout>
+    <div class="">
+        <div class="max-w-7xl mx-auto">
+            <div class="shadow-sm sm:rounded-lg">
+                    <x-carousel :slides="$slides" />
+
+            </div>
+        </div>
+    </div>
+</x-public-layout>
