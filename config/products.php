08261151214d2ef436aa3bb437e16312d5ef6918
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-07 12:27:19 +0100

    feat: added products&slides to config

---
+++ config/products.php
@@ -0,0 +1,69 @@
+<?php
+
+return [
+    'products' => [
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.99",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.98",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.97",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.96",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.95",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.94",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.93",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.92",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+        [
+            'imgSrc' => "https://penguinui.s3.amazonaws.com/component-assets/card-img-3.webp",
+            'imgAlt' => "CASIO G-SHOCK GA2100, Black face, black bands",
+            'name' => "CASIO G-SHOCK GA2100",
+            'price' => "$99.91",
+            'description' => "The Casio G-Shock GA2100 is simply designed for easy timekeeping, featuring a sleek profile and clear display.",
+        ],
+    ],
+];
