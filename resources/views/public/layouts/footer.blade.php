f9afdd5a07ddddd58e5451f34416005a77414ac4
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-10 02:15:11 +0100

    feat: added public footer

---
+++ resources/views/public/layouts/footer.blade.php
@@ -0,0 +1,61 @@
+<!-- footer -->
+<section class="min-w-screen h-[40vh] bg-slate-900 mt-8 pl-1">
+    <div class="flex justify-center h-full gap-8 max-w-7xl mx-auto">
+        <div class="w-full h-full justify-center p-6 gap-6 text-gray-100 flex flex-col">
+            <x-application-logo />
+            <p class="flex items-center"><span class="material-symbols-outlined small-icon">call</span>
+                +123456789
+            </p>
+            <p class="flex items-center"><span class="material-symbols-outlined small-icon">email</span>
+                contact@bestshop.com
+            </p>
+            <p class="flex items-center"><span class="material-symbols-outlined small-icon">location_on</span>
+                Budapest, Hungary
+            </p>
+            <div class="flex items-center gap-2">
+                <div class="bg-slate-600 p-2.5 rounded-full cursor-pointer">
+                    <svg class="size-4 fill-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M80 299.3V512H196V299.3h86.5l18-97.8H196V166.9c0-51.7 20.3-71.5 72.7-71.5c16.3 0 29.4 .4 37 1.2V7.9C291.4 4 256.4 0 236.2 0C129.3 0 80 50.5 80 159.4v42.1H14v97.8H80z"/></svg>
+                </div>
+                <div class="bg-slate-600 p-2.5 rounded-full cursor-pointer">
+                    <svg class="size-4 fill-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                </div>
+                <div class="bg-slate-600 p-2.5 rounded-full cursor-pointer">
+                    <svg class="size-4 fill-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M224.1 141c-63.6 0-114.9 51.3-114.9 114.9s51.3 114.9 114.9 114.9S339 319.5 339 255.9 287.7 141 224.1 141zm0 189.6c-41.1 0-74.7-33.5-74.7-74.7s33.5-74.7 74.7-74.7 74.7 33.5 74.7 74.7-33.6 74.7-74.7 74.7zm146.4-194.3c0 14.9-12 26.8-26.8 26.8-14.9 0-26.8-12-26.8-26.8s12-26.8 26.8-26.8 26.8 12 26.8 26.8zm76.1 27.2c-1.7-35.9-9.9-67.7-36.2-93.9-26.2-26.2-58-34.4-93.9-36.2-37-2.1-147.9-2.1-184.9 0-35.8 1.7-67.6 9.9-93.9 36.1s-34.4 58-36.2 93.9c-2.1 37-2.1 147.9 0 184.9 1.7 35.9 9.9 67.7 36.2 93.9s58 34.4 93.9 36.2c37 2.1 147.9 2.1 184.9 0 35.9-1.7 67.7-9.9 93.9-36.2 26.2-26.2 34.4-58 36.2-93.9 2.1-37 2.1-147.8 0-184.8zM398.8 388c-7.8 19.6-22.9 34.7-42.6 42.6-29.5 11.7-99.5 9-132.1 9s-102.7 2.6-132.1-9c-19.6-7.8-34.7-22.9-42.6-42.6-11.7-29.5-9-99.5-9-132.1s-2.6-102.7 9-132.1c7.8-19.6 22.9-34.7 42.6-42.6 29.5-11.7 99.5-9 132.1-9s102.7-2.6 132.1 9c19.6 7.8 34.7 22.9 42.6 42.6 11.7 29.5 9 99.5 9 132.1s2.7 102.7-9 132.1z"/></svg>
+                </div>
+                <div class="bg-slate-600 p-2.5 rounded-full cursor-pointer">
+                    <svg class="size-4 fill-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M380.9 97.1C339 55.1 283.2 32 223.9 32c-122.4 0-222 99.6-222 222 0 39.1 10.2 77.3 29.6 111L0 480l117.7-30.9c32.4 17.7 68.9 27 106.1 27h.1c122.3 0 224.1-99.6 224.1-222 0-59.3-25.2-115-67.1-157zm-157 341.6c-33.2 0-65.7-8.9-94-25.7l-6.7-4-69.8 18.3L72 359.2l-4.4-7c-18.5-29.4-28.2-63.3-28.2-98.2 0-101.7 82.8-184.5 184.6-184.5 49.3 0 95.6 19.2 130.4 54.1 34.8 34.9 56.2 81.2 56.1 130.5 0 101.8-84.9 184.6-186.6 184.6zm101.2-138.2c-5.5-2.8-32.8-16.2-37.9-18-5.1-1.9-8.8-2.8-12.5 2.8-3.7 5.6-14.3 18-17.6 21.8-3.2 3.7-6.5 4.2-12 1.4-32.6-16.3-54-29.1-75.5-66-5.7-9.8 5.7-9.1 16.3-30.3 1.8-3.7 .9-6.9-.5-9.7-1.4-2.8-12.5-30.1-17.1-41.2-4.5-10.8-9.1-9.3-12.5-9.5-3.2-.2-6.9-.2-10.6-.2-3.7 0-9.7 1.4-14.8 6.9-5.1 5.6-19.4 19-19.4 46.3 0 27.3 19.9 53.7 22.6 57.4 2.8 3.7 39.1 59.7 94.8 83.8 35.2 15.2 49 16.5 66.6 13.9 10.7-1.6 32.8-13.4 37.4-26.4 4.6-13 4.6-24.1 3.2-26.4-1.3-2.5-5-3.9-10.5-6.6z"/></svg>
+                </div>
+                <div class="bg-slate-600 p-2.5 rounded-full cursor-pointer">
+                   <svg class="size-4 fill-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M204 6.5C101.4 6.5 0 74.9 0 185.6 0 256 39.6 296 63.6 296c9.9 0 15.6-27.6 15.6-35.4 0-9.3-23.7-29.1-23.7-67.8 0-80.4 61.2-137.4 140.4-137.4 68.1 0 118.5 38.7 118.5 109.8 0 53.1-21.3 152.7-90.3 152.7-24.9 0-46.2-18-46.2-43.8 0-37.8 26.4-74.4 26.4-113.4 0-66.2-93.9-54.2-93.9 25.8 0 16.8 2.1 35.4 9.6 50.7-13.8 59.4-42 147.9-42 209.1 0 18.9 2.7 37.5 4.5 56.4 3.4 3.8 1.7 3.4 6.9 1.5 50.4-69 48.6-82.5 71.4-172.8 12.3 23.4 44.1 36 69.3 36 106.2 0 153.9-103.5 153.9-196.8C384 71.3 298.2 6.5 204 6.5z"/></svg>
+                </div>
+                <div class="bg-slate-600 p-2.5 rounded-full cursor-pointer">
+                    <svg class="size-4 fill-gray-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--!Font Awesome Free 6.7.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.--><path d="M448 209.9a210.1 210.1 0 0 1 -122.8-39.3V349.4A162.6 162.6 0 1 1 185 188.3V278.2a74.6 74.6 0 1 0 52.2 71.2V0l88 0a121.2 121.2 0 0 0 1.9 22.2h0A122.2 122.2 0 0 0 381 102.4a121.4 121.4 0 0 0 67 20.1z"/></svg>
+                </div>
+            </div>
+        </div>
+        <div class="w-full h-full justify-center p-6 gap-6 text-gray-100 flex flex-col">
+            <h3 class="text-pretty font-bold text-xl underline underline-offset-4">Company</h3>
+            <ul>
+                <li class="text-lg">FAQ</li>
+                <li class="text-lg">About us</li>
+                <li class="text-lg">Career</li>
+                <li class="text-lg">Contact Us</li>
+                <li class="text-lg">Affiliate</li>
+                <li class="text-lg">Order History</li>
+            </ul>
+        </div>
+        <div class="w-full h-full justify-center p-6 gap-6 text-gray-100 flex flex-col">
+            <h1 class="text-2xl font-extrabold text-pretty text-gray-100">Subscribe To Our Newsletter</h1>
+            <p class="text-md text-pretty font-semibold text-gray-100">Get all the latest information on Events, Sales and Offers. Do not miss our huge sales!</p>
+            <x-text-input />
+            <x-primary-button>
+                Subscribe
+            </x-primary-button>
+            <p class="text-md text-gray-100 -mb-4">We accept Safe Payment from:</p>
+            <img src="{{ asset('storage/home/cards/credit.png') }}" class="h-auto w-36">
+        </div>
+    </div>
+    <div class="min-w-screen bg-slate-700 text-gray-100 text-center -ml-1 py-2">
+        <p>Copyright &copy; 2025 BestShop. All Rights Reserved.</p>
+    </div>
+</section>
