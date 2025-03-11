e87234534e2bacf2e4534cf974fb1417fd91c06d
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-11 02:14:02 +0100

    refactor: changed update handling

--- app/Http/Controllers/UserProfileController.php
+++ app/Http/Controllers/UserProfileController.php
@@ -34,19 +34,20 @@ class UserProfileController extends Controller
      */
     public function update(ProfileUpdateRequest $request): RedirectResponse
     {
-        $request->user()->fill($request->validated());
+        $user = $request->user();
+        $user->fill($request->validated());
 
         if ($request->hasFile('image')) {
-            $this->imageService->delete($request->user()->image);
-            $request->user()->image = $this->imageService->upload($request->file('image'), 'profile_images');
+            $this->imageService->delete($user->image);
+            $user->image = $this->imageService->upload($request->file('image'), 'profile_images');
         }
 
 
-        if ($request->user()->isDirty('email')) {
-            $request->user()->email_verified_at = null;
+        if ($user->isDirty('email')) {
+            $user->email_verified_at = null;
         }
 
-        $request->user()->save();
+        $user->save();
 
         return Redirect::route('user.profile.edit')->with('status', 'profile-updated');
     }
