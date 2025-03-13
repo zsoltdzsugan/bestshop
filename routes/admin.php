6770aafaa971ae38e9bc18787fe51fcbd0ba3b89
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-13 15:14:53 +0100

    feat: SubCategory & ChildCategory routes

--- routes/admin.php
+++ routes/admin.php
@@ -3,7 +3,9 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminProfileController;
 use App\Http\Controllers\Backend\CategoryController;
+use App\Http\Controllers\Backend\ChildCategoryController;
 use App\Http\Controllers\Backend\SliderController;
+use App\Http\Controllers\Backend\SubCategoryController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'role:admin'])
@@ -19,6 +21,9 @@ Route::middleware(['auth', 'role:admin'])
         Route::resource('/slider', SliderController::class);
 
         Route::resource('/category', CategoryController::class);
+        Route::resource('/sub-category', SubCategoryController::class);
+        Route::get('/get-subcategory', [ChildCategoryController::class, 'getSubCategories'])->name('get-subcategories');
+        Route::resource('/child-category', ChildCategoryController::class);
     });
 
 Route::get('/admin/login', [AdminController::class, 'login'])->name('admin.login');
