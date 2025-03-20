5f0358570d785bb8c9cfa54cad3e3d091b551eaa
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-20 04:41:42 +0100

    feat: helper function active sidebar item

---
+++ app/Helpers/helper.php
@@ -0,0 +1,14 @@
+<?php
+
+/** set admin sidebar item active */
+function setActive(array $routes)
+{
+    if (is_array($routes)) {
+        foreach ($routes as $route) {
+            if (request()->routeIs($route)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
