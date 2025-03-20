1f0243010394fc673f3410a15a9c9bc7d75e3397
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-03-20 22:02:13 +0100

    refactor: subcategory dropdown reusable

---
+++ resources/js/subCategoryDropdown.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const categorySelect = document.querySelector('#category_id');
+
+    categorySelect.addEventListener('change', function () {
+        let id = this.value;
+
+        fetch(`/admin/get-subcategory?id=${id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log("DATA", data);
+                const subCategorySelect = document.querySelector('#sub-category_id');
+                const defaultOption = document.createElement('option');
+
+                subCategorySelect.innerHTML = '';
+                defaultOption.disabled = true;
+                defaultOption.selected = true;
+                defaultOption.textContent = data.length > 0 ? "Please Select" : "No Subcategory";
+                defaultOption.value = "";
+                subCategorySelect.appendChild(defaultOption);
+
+                data.forEach(function (item) {
+
+                    const option = document.createElement('option');
+                    option.value = item.id;
+                    option.textContent = item.name;
+
+                    subCategorySelect.appendChild(option);
+                });
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+    });
+});
