0ff17cfc30925e685fc48c09aa3a7505024d5257
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-03 00:22:27 +0200

    feat: vendor product controller

--- app/Http/Controllers/Backend/Vendor/ProductController.php
+++ app/Http/Controllers/Backend/Vendor/ProductController.php
@@ -3,10 +3,23 @@
 namespace App\Http\Controllers\Backend\Vendor;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Backend\ProductStoreRequest;
+use App\Models\Product;
+use App\Services\ImageService;
+use Illuminate\Container\Attributes\Auth;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Str;
 
 class ProductController extends Controller
 {
+    protected ImageService $imageService;
+
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -26,9 +39,26 @@ class ProductController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request): RedirectResponse
     {
-        //
+        $data = $request->validated();
+
+        $data['is_top'] = $request->has('is_top') ? 1 : 0;
+        $data['is_new'] = $request->has('is_new') ? 1 : 0;
+        $data['is_best'] = $request->has('is_best') ? 1 : 0;
+        $data['is_featured'] = $request->has('is_featured') ? 1 : 0;
+        $data['is_approved'] = 0;
+
+        if ($request->hasFile('thumb_image')) {
+            $data['thumb_image'] = $this->imageService->upload($request->file('thumb_image'), 'product_thumb_images');
+        }
+
+        $product = new Product();
+        $product->fill($data);
+        $product->slug = Str::slug($request->name);
+        $product->save();
+
+        return redirect()->route('admin.product.index')->with('status', 'product-created');
     }
 
     /**
