9da01a07533bd8ae21e16560c4c53256d1822794
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-03 00:21:24 +0200

    feat: product controller

--- app/Http/Controllers/Backend/Admin/ProductController.php
+++ app/Http/Controllers/Backend/Admin/ProductController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers\Backend\Admin;
 
 use App\DataTables\ProductDataTable;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Backend\ProductStoreRequest;
+use App\Models\Brand;
 use App\Models\Category;
 use App\Models\Product;
+use App\Models\Product\ImageGallery;
+use App\Models\Vendor;
+use App\Services\ImageService;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Str;
 
 class ProductController extends Controller
 {
+    protected ImageService $imageService;
+
+    public function __construct(ImageService $imageService)
+    {
+        $this->imageService = $imageService;
+    }
+
     /**
      * Display a listing of the resource.
      */
@@ -25,15 +39,34 @@ class ProductController extends Controller
     public function create(): View
     {
         $categories = Category::all();
-        return view('admin.product.create', compact('categories'));
+        $vendors = Vendor::all();
+        $brands = Brand::all();
+        return view('admin.product.create', compact('categories', 'vendors', 'brands'));
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(ProductStoreRequest $request): RedirectResponse
     {
-        //
+        $data = $request->validated();
+
+        $data['is_top'] = $request->has('is_top') ? 1 : 0;
+        $data['is_new'] = $request->has('is_new') ? 1 : 0;
+        $data['is_best'] = $request->has('is_best') ? 1 : 0;
+        $data['is_featured'] = $request->has('is_featured') ? 1 : 0;
+        $data['is_approved'] = $request->has('is_approved') ? 1 : 0;
+
+        if ($request->hasFile('thumb_image')) {
+            $data['thumb_image'] = $this->imageService->upload($request->file('thumb_image'), 'product-images');
+        }
+
+        $product = new Product();
+        $product->fill($data);
+        $product->slug = Str::slug($request->name);
+        $product->save();
+
+        return redirect()->route('admin.product.index')->with('status', 'product-created');
     }
 
     /**
@@ -50,15 +83,36 @@ class ProductController extends Controller
     public function edit(string $id): View
     {
         $product = Product::findOrFail($id);
-        return view('admin.profile.edit', compact('product'));
+        $vendors = Vendor::all();
+        $categories = Category::all();
+        $brands = Brand::all();
+        return view('admin.product.edit', compact('product', 'vendors', 'categories', 'brands'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(ProductStoreRequest $request, string $id): RedirectResponse
     {
-        //
+        $data = $request->validated();
+        $product = Product::findOrFail($id);
+
+        $data['is_top'] = $request->has('is_top') ? 1 : 0;
+        $data['is_new'] = $request->has('is_new') ? 1 : 0;
+        $data['is_best'] = $request->has('is_best') ? 1 : 0;
+        $data['is_featured'] = $request->has('is_featured') ? 1 : 0;
+        $data['is_approved'] = $request->has('is_approved') ? 1 : 0;
+
+        if ($request->hasFile('thumb_image')) {
+            $this->imageService->delete($product->thumb_image);
+            $data['thumb_image'] = $this->imageService->upload($request->file('thumb_image'), 'product-images');
+        }
+
+        $product->fill($data);
+        $product->slug = Str::slug($request->name);
+        $product->save();
+
+        return redirect()->route('admin.product.index')->with('status', 'product-updated');
     }
 
     /**
@@ -66,6 +120,20 @@ class ProductController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        $product = Product::findOrFail($id);
+        $productImages = ImageGallery::where('product_id', $id)->get();
+
+        $this->imageService->delete($product->thumb_image);
+
+        $imagePaths = [];
+        foreach ($productImages as $image) {
+            $imagePaths[] = $image->image;
+        }
+
+        $this->imageService->deleteMultiple($imagePaths);
+
+        $product->delete();
+
+        return redirect()->route('admin.product.index')->with('status', 'product-deleted');
     }
 }
