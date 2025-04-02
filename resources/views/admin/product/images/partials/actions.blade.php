db8001e9594c1e2adc5e9d39ce62677eae23f646
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-03 00:23:43 +0200

    feat: product image gallery views

---
+++ resources/views/admin/product/images/partials/actions.blade.php
@@ -0,0 +1,35 @@
+<div class="flex space-x-2">
+    <!-- Delete Button -->
+    <x-danger-button
+        x-data=""
+        x-on:click.prevent="$dispatch('open-modal', 'confirm-product-deletion-{{ $image->id }}')"
+    >
+        <i class="fa-solid fa-trash"></i>
+    </x-danger-button>
+
+    <!-- Delete Confirmation Modal -->
+    <x-modal name="confirm-product-deletion-{{ $image->id }}" focusable>
+        <form method="POST" action="{{ route('admin.product.images.destroy', ['product' => $image->product_id, 'image' => $image->id]) }}" class="p-6">
+            @csrf
+            @method('DELETE')
+
+            <h2 class="text-lg font-medium text-gray-900 dark:text-gray-100">
+                {{ __('Are you sure you want to delete this product?') }}
+            </h2>
+
+            <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
+                {{ __('Once deleted, this action cannot be undone.') }}
+            </p>
+
+            <div class="mt-6 flex justify-end">
+                <x-secondary-button x-on:click="$dispatch('close')">
+                    {{ __('Cancel') }}
+                </x-secondary-button>
+
+                <x-danger-button class="ms-3">
+                    {{ __('Delete') }}
+                </x-danger-button>
+            </div>
+        </form>
+    </x-modal>
+</div>
---
+++ resources/views/admin/product/images/partials/create.blade.php
@@ -0,0 +1,54 @@
+<section>
+    <form id="send-verification" method="post" action="{{ route('verification.send') }}">
+        @csrf
+    </form>
+
+    <form method="post" action="{{ route('admin.product.images.store', $product->id) }}" enctype="multipart/form-data" class="space-y-6">
+        @csrf
+
+        <div>
+            <x-input-label :value="__('Preview')" />
+            <img class="w-60 max-h-60" name="new-preview" id="new-preview" />
+            <x-input-error class="mt-2" :messages="$errors->get('new-preview')" />
+        </div>
+        <div>
+            <x-input-label for="image" :value="__('Images')" />
+            <x-text-input id="image" name="image[]" type="file" multiple class="mt-1 block w-full p-2" onchange="previewImage(event)"/>
+            <x-text-input hidden id="product_id" name="product_id" type="text" value="{{ old('product_id', $product->id) }}" />
+            <x-input-error class="mt-2" :messages="$errors->get('thumb_image')" />
+        </div>
+
+        <div class="flex items-center gap-4">
+            <x-primary-button>{{ __('Create') }}</x-primary-button>
+
+            @if (session('status') === 'product-images-added')
+                <p
+                    x-data="{ show: true }"
+                    x-show="show"
+                    x-transition
+                    x-init="setTimeout(() => show = false, 2000)"
+                    class="text-sm text-gray-600 dark:text-gray-400"
+                >{{ __('Upload.') }}</p>
+            @endif
+        </div>
+    </form>
+</section>
+@push('scripts')
+    <script>
+        function previewImage(event) {
+            const file = event.target.files[0];
+            const preview = document.getElementById('new-preview');
+
+            if (file) {
+                const reader = new FileReader();
+                reader.onload = function(e) {
+                    preview.src = e.target.result;
+                    preview.classList.remove('hidden');
+                };
+                reader.readAsDataURL(file);
+            }
+        }
+    </script>
+    @vite('resources/js/subCategoryDropdown.js')
+    @vite('resources/js/childCategoryDropdown.js')
+@endpush
