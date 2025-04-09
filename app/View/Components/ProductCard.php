a8b5e9d7aafc94cd2d5d60a5743f2dea7e609ba7
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-10 01:06:17 +0200

    refactor: productcard with vatiant

--- app/View/Components/ProductCard.php
+++ app/View/Components/ProductCard.php
@@ -8,12 +8,57 @@ use Illuminate\View\Component;
 
 class ProductCard extends Component
 {
+    public string $variant;
+
+    public ?string $href;
+
     /**
      * Create a new component instance.
      */
-    public function __construct()
+    public function __construct(string $variant = 'default', ?string $href = null)
     {
-        //
+        $this->variant = $variant;
+        $this->href = $href;
+    }
+
+    public function imageContainerClasses(): string
+    {
+        $variants = [
+            'compact' => 'w-full',
+            'default' => 'h-44 md:h-full',
+        ];
+
+        return $variants[$this->variant] ?? $variants['default'];
+    }
+
+    public function contentContainerClasses(): string
+    {
+        $variants = [
+            'compact' => 'px-2',
+            'default' => 'p-6 max-h-56 h-full',
+        ];
+
+        return $variants[$this->variant] ?? $variants['default'];
+    }
+
+    public function headerClasses(): string
+    {
+        $variants = [
+            'compact' => 'my-4 flex-col',
+            'default' => 'max-h-52 h-full gap-4 md:gap-12 md:flex-row',
+        ];
+
+        return $variants[$this->variant] ?? $variants['default'];
+    }
+
+    public function titleClasses(): string
+    {
+        $variants = [
+            'compact' => 'text-sm lg:text-md',
+            'default' => 'text-lg lg:text-xl font-bold ',
+        ];
+
+        return $variants[$this->variant] ?? $variants['default'];
     }
 
     /**
--- resources/views/components/product-card.blade.php
+++ resources/views/components/product-card.blade.php
@@ -1,47 +1,75 @@
 @props(['product'])
+@php
+    use Carbon\Carbon;
 
-    <article class="group flex rounded-radius max-w-sm flex-col overflow-hidden border border-outline bg-surface text-on-surface dark:border-outline-dark dark:bg-surface-dark dark:text-on-surface-dark">
-        <!-- Image -->
-        <div class="h-44 md:h-64 overflow-hidden">
-            <img :src="product.imgSrc" class="object-contain transition duration-700 ease-out group-hover:scale-105" :alt="product.imgAlt" />
-        </div>
-        <!-- Content -->
-        <div class="flex flex-col gap-4 p-6">
-            <!-- Header -->
-            <div class="flex flex-col md:flex-row gap-4 md:gap-12 justify-between">
-                <!-- Title & Rating -->
-                <div class="flex flex-col">
-                    <h3 class="sm:text-md lg:text-lg font-bold text-on-surface-strong dark:text-on-surface-dark-strong" aria-describedby="productDescription" x-text="product.name"></h3>
-                    <!-- Rating -->
-                    <div class="flex items-center gap-1">
-                        <span class="sr-only">Rated 3 stars</span>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-amber-500" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-amber-500" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-amber-500" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-on-surface/50 dark:text-on-surface-dark/50" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                        </svg>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-on-surface/50 dark:text-on-surface-dark/50" aria-hidden="true">
-                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                        </svg>
-                    </div>
+    $isOnSale = $product->sale_price &&
+        (Carbon::parse($product->sale_start)->lessThanOrEqualTo(Carbon::today())
+        && Carbon::today()->lessThanOrEqualTo(Carbon::parse($product->sale_end)));
+
+    if ($isOnSale) {
+        $percent = round((($product->price - $product->sale_price) / $product->price) * 100, 0);
+    }
+@endphp
+
+<article class="group flex rounded-radius h-full flex-col overflow-hidden bg-surface text-on-surface dark:bg-surface-dark dark:text-on-surface-dark">
+    <!-- Image -->
+    <div class="relative overflow-hidden {{ $imageContainerClasses() }}">
+        <img src="{{ asset('storage/'.$product->thumb_image) }}" class="object-cover w-full h-auto transition duration-700 ease-out group-hover:scale-105" alt="{{ $product->name }}" />
+        {{-- TODO: to component --}}
+        @if ($product->is_top)
+            <div class="font-bold group-hover:opacity-0 transition-all duration-300 ease-in-out absolute max-w-12 w-full top-2 left-2 p-1.5 bg-primary dark:bg-primary-dark text-on-primary dark:text-on-primary-dark flex items-center justify-center text-center">Top</div>
+        @endif
+        @if ($product->is_new)
+            <div class="font-bold group-hover:opacity-0 transition-all duration-300 ease-in-out absolute max-w-12 w-full {{ $product->is_top ? 'top-12' : 'top-2' }} left-2 p-1.5 bg-primary dark:bg-primary-dark text-on-primary dark:text-on-primary-dark flex items-center justify-center text-center">New</div>
+        @endif
+        @if ($isOnSale)
+            <div class="font-bold group-hover:opacity-0 transition-all duration-300 ease-in-out absolute max-w-12 w-full top-2 right-2 p-1.5 bg-danger flex items-center justify-center text-center">-{{ $percent }}<span class="text-xs font-bold">%</span></div>
+        @endif
+    </div>
+    <!-- Content -->
+    <div class="flex flex-col gap-4 {{ $contentContainerClasses }}">
+        <!-- Header -->
+        <div class="flex justify-between {{ $headerClasses }}">
+            <!-- Title & Rating -->
+            <div class="flex flex-col gap-1">
+                <h3 class="{{ $titleClasses() }} text-on-surface-strong dark:text-on-surface-dark-strong" aria-describedby="{{ $product->name }}">{{ $product->name }}</h3>
+                <!-- Rating -->
+                <div class="flex items-center gap-1">
+                    <span class="sr-only">Rated 3 stars</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-amber-500" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-amber-500" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-amber-500" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-on-surface/50 dark:text-on-surface-dark/50" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                    </svg>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-4 text-on-surface/50 dark:text-on-surface-dark/50" aria-hidden="true">
+                        <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                    </svg>
                 </div>
-                <span class="text-xl" x-text="product.price"><span class="sr-only">Price</span></span>
             </div>
-            <p id="productDescription" class="mb-2 text-pretty text-sm" x-text="product.description">
-            </p>
+            <!-- Price -->
+            <div class="flex {{ $variant === 'default' ? 'flex-col' : 'flex-row items-center mt-4 gap-2' }}">
+                <span class="text-xl text-on-surface-strong dark:text-on-surface-dark-strong"><span class="sr-only">Price</span>${{ $isOnSale ? $product->sale_price : $product->price }}</span>
+                @if ($isOnSale)
+                    <span class="text-md text-center text-danger brightness-75 line-through {{ $variant === 'default' ? '' : 'mt-0.5' }}"><span class="sr-only">Sale Price</span>${{ $product->price }}</span>
+                @endif
+            </div>
+        </div>
+        @if ($variant === 'default')
+            <p id="productDescription" class="flex-end mb-2 text-pretty text-sm shrink-0 min-h-10 truncate overflow-ellipsis">{{ $product->short_description }}</p>
             <!-- Button -->
-            <button type="button" class="flex items-center justify-center gap-2 whitespace-nowrap bg-primary px-4 py-2 text-center text-sm font-medium tracking-wide text-on-primary transition hover:opacity-75 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary active:opacity-100 active:outline-offset-0 dark:bg-primary-dark dark:text-on-primary-dark dark:focus-visible:outline-primary-dark rounded-radius">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" class="size-3.5">
+            <x-button type="button" class="flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden="true" class="size-4">
                     <path fill-rule="evenodd" d="M5 4a3 3 0 0 1 6 0v1h.643a1.5 1.5 0 0 1 1.492 1.35l.7 7A1.5 1.5 0 0 1 12.342 15H3.657a1.5 1.5 0 0 1-1.492-1.65l.7-7A1.5 1.5 0 0 1 4.357 5H5V4Zm4.5 0v1h-3V4a1.5 1.5 0 0 1 3 0Zm-3 3.75a.75.75 0 0 0-1.5 0v1a3 3 0 1 0 6 0v-1a.75.75 0 0 0-1.5 0v1a1.5 1.5 0 1 1-3 0v-1Z" clip-rule="evenodd" />
                 </svg>
-                Add to Cart
-            </button>
-        </div>
-    </article>
+                <p class="mt-0.5">Add to Cart</p>
+            </x-button>
+        @endif
+    </div>
+</article>
