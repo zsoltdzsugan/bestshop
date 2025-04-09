63d86710d63bfe31605558f4eea57185c0d63888
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-04-10 01:12:26 +0200

    refactor: layout

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -1,8 +1,15 @@
 <x-app-layout>
-    <div class="min-h-screen max-w-screen mx-auto rounded-radius">
+    <div class="min-h-screen max-w-screen mx-auto rounded-radius text-on-surface dark:text-on-surface-dark">
         @include('public.layouts.search')
         @include('public.layouts.navigation')
 
+        @isset($header)
+            <header class="shadow rounded-radius my-12">
+                <div class="max-w-7xl mx-auto bg-surface/25 dark:bg-surface-dark/25 border border-outline dark:border-outline-dark">
+                    {{ $header }}
+                </div>
+            </header>
+        @endisset
         <!-- Page Content -->
         <main class="max-w-7xl flex flex-col space-y-12 my-12 mx-auto">
             {{ $slot }}
